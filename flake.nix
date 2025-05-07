cd6a3474baa9217c8c80dcf05c0a13bc40fa325e
ofrades <ofrades@pm.me>
2025-05-08 00:42:39 +0100

    update :)

--- flake.nix
+++ flake.nix
@@ -1,61 +1,48 @@
 {
   description = "LaTeX document environment with GregorioTeX support";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-
-        # Custom LaTeX packages with all required dependencies
         texlive = pkgs.texlive.combine {
           inherit (pkgs.texlive)
-            scheme-medium # Base TeX Live scheme
-            # KOMA-Script packages
-            koma-script
-            # Required packages from your document
-            background gregoriotex adjmulticol adforn hyperref tikz
-            # Add any other packages you need
-            latexmk # For build automation
-            latexindent # For formatting
-          ;
+          # Base scheme that includes many common packages
+            scheme-full
+            # Only include packages not in scheme-medium
+            collection-fontsextra # Extra fonts including many needed for classical texts
+            collection-latexextra # For advanced LaTeX features
+            # Specific packages you need that aren't in the collections above
+            returntogrid gregoriotex luacode tikz-cd latexindent ebgaramond
+            lettrine paracol polyglossia fancyhdr fontspec;
         };
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = [
             texlive
             # Tools for development
-            pkgs.tectonic # Alternative TeX engine
-            pkgs.biber # Bibliography processor
-            pkgs.texlab # LSP server for TeX
+            pkgs.tectonic
+            pkgs.biber
+            pkgs.texlab
           ];
-
-          # Set up environment variables
           shellHook = ''
             echo "LaTeX development environment loaded"
             echo "Use 'latexmk -pdf main.tex' to build your document"
           '';
         };
-
-        # Add a package definition to build the document
         packages.default = pkgs.stdenvNoCC.mkDerivation {
           name = "missal-tradicional";
           src = ./.;
-
           buildInputs = [ texlive ];
-
           buildPhase = ''
-            # Run latexmk to build the PDF
-            latexmk -pdf main.tex
+            latexmk -pdflatex=lualatex -pdf --shell-escape -f -quiet Missal.tex
           '';
-
           installPhase = ''
             mkdir -p $out
-            cp main.pdf $out/
+            cp *.pdf $out/
           '';
         };
       });
